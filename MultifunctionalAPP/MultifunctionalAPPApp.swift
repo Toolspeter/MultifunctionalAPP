fed31d5fcdbc67964ab1a6db121ddb30c92e7f3e
Toolspeter <hua01jan@outlook.com>
2024-12-29 00:08:37 +0800

    This is a multi-functional APP including mini games and practical tools

---
+++ MultifunctionalAPP/MultifunctionalAPPApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultifunctionalAPPApp.swift
+//  MultifunctionalAPP
+//
+//  Created by Peter Chen on 2024/12/29.
+//
+
+import SwiftUI
+
+@main
+struct MultifunctionalAPPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MultifunctionalAPPTests/MultifunctionalAPPTests.swift
@@ -0,0 +1,16 @@
+//
+//  MultifunctionalAPPTests.swift
+//  MultifunctionalAPPTests
+//
+//  Created by Peter Chen on 2024/12/29.
+//
+
+import Testing
+
+struct MultifunctionalAPPTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
